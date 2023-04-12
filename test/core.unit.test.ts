7c27691bdc634d745f85beac34dc8909485124da
Mihail <mihail.pe@outlook.com>
2023-04-12 18:28:48 +0300

    Add unit tests for the core
    Using @testdeck/mocha

---
+++ test/core.unit.test.ts
@@ -0,0 +1,111 @@
+import {
+  Point,
+  Line,
+  Triangle,
+  arePointsEqual,
+  areLinesEqual,
+  isTrianglesEqual,
+} from "../src/core";
+import { suite, test } from "@testdeck/mocha";
+import * as _chai from "chai";
+import { expect } from "chai";
+
+_chai.should();
+_chai.expect;
+
+@suite
+class PointTests {
+  @test
+  "arePointsEqual returns true for equal points"() {
+    const p1 = new Point(0, 0);
+    const p2 = new Point(0.01, 0.03);
+    expect(arePointsEqual(p1, p2, 0.1)).to.be.true;
+  }
+
+  @test
+  "arePointsEqual returns false for non-equal points"() {
+    const p1 = new Point(0, 0);
+    const p2 = new Point(1.3, 0.1);
+    expect(arePointsEqual(p1, p2, 1.1)).to.be.false;
+  }
+
+  @test
+  "arePointsEqual returns true for equal points with default tolerance"() {
+    const p1 = new Point(0, 0);
+    const p2 = new Point(0.1, 0.1);
+    expect(arePointsEqual(p1, p2)).to.be.true;
+  }
+
+  @test
+  public "clone method should return a new point with the same coordinates"(): void {
+    // Arrange
+    const originalPoint = new Point(1, 2);
+
+    // Act
+    const clonedPoint = originalPoint.clone();
+
+    // Assert
+    expect(clonedPoint).to.not.equal(originalPoint);
+    expect(clonedPoint.x).to.equal(originalPoint.x);
+    expect(clonedPoint.y).to.equal(originalPoint.y);
+  }
+}
+
+@suite
+class LineTests {
+  @test
+  "should return true for equal lines"() {
+    // Arrange
+    const p1 = new Point(0, 0);
+    const p2 = new Point(1, 1);
+    const lineA = new Line(p1, p2);
+    const lineB = new Line(p1.clone(), p2.clone()); // clone to get a new reference
+
+    // Act
+    const result = areLinesEqual(lineA, lineB);
+
+    // Assert
+    expect(result).to.be.true;
+  }
+
+  @test
+  "should return true for collinear lines"() {
+    // Arrange
+    const p1 = new Point(0, 0);
+    const p2 = new Point(1, 1);
+    const lineA = new Line(p1, p2);
+    const lineB = new Line(p2, p1);
+
+    // Act
+    const result = areLinesEqual(lineA, lineB);
+
+    // Assert
+    expect(result).to.be.true;
+  }
+}
+
+@suite
+class TriangleTests {
+  @test
+  "returns true for equal triangles"() {
+    const p1 = new Point(0, 0);
+    const p2 = new Point(5, 0);
+    const p3 = new Point(0, 5);
+    const t1 = new Triangle(p1, p2, p3);
+    const t2 = new Triangle(p3, p1, p2);
+
+    const result = isTrianglesEqual(t1, t2);
+
+    expect(result).to.be.true;
+  }
+
+  @test
+  "returns false for non-equal triangles"() {
+    const t1 = new Triangle(new Point(0, 0), new Point(5, 0), new Point(0, 5));
+    const t2 = new Triangle(new Point(0, 0), new Point(5, 5), new Point(0, 5));
+
+    const result = isTrianglesEqual(t1, t2);
+
+    expect(result).to.be.false;
+  }
+}
